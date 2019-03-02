b01a8cc5193a9118c0e7d8667a7cc8fbdfc576a6
Damien Gros <damien.gr34@gmail.com>
2019-03-02 11:32:11 +0100

    test netlify deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header";
 export default () => (
     <div style={{color: 'teal'}}>
         <Header headerText="Home"/>
-        <p>Lorem ipsum</p>
+        <p>Lorem ipsum!</p>
         <div><Link to="/about/">About</Link></div>
         <div><Link to="/contact/">Contact</Link></div>
     </div>
